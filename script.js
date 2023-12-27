7b45cced64f2a9faeff1d6f91e7501e6fa97c174
Subhrajit <subhrajittalukdar411@gmail.com>
2023-12-27 16:03:07 +0530

    added a section and some minor css modifications

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ var navLinks = document.getElementById("navLinks");
       // Attach the toggleNav function to the click event of the hamburger and close icons
       document.querySelector('.bi.bi-list').addEventListener('click', toggleNav);
       document.querySelector('.bi.bi-x-lg').addEventListener('click', toggleNav);
+      
+    //   scroll down animation
+    document.addEventListener('DOMContentLoaded', function () {
+        let welcomeContainer = document.querySelector('.welcome');
+
+        function handleScroll() {
+            let scrollPosition = window.scrollY;
+            let elementPosition = welcomeContainer.offsetTop;
+
+            // Adjust the threshold value based on when you want the animation to start
+            let threshold = window.innerHeight / 2;
+
+            if (scrollPosition >= elementPosition - threshold) {
+                welcomeContainer.style.opacity = '1';
+                welcomeContainer.style.transform = 'translateY(0)';
+            }
+        }
+
+        // Initial check on page load
+        handleScroll();
+
+        // Attach the handleScroll function to the scroll event
+        window.addEventListener('scroll', handleScroll);
+    });
+
+    // carousel for whats latest
+    $(document).ready(function(){
+        $("#carouselExampleIndicators").carousel({
+          interval: 2000,
+          pause: "hover"
+        });
+      });
+
+    
+    
