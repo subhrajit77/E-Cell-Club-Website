d966d8086c9b1d5c41815526d3ad22bd7c038ad6
Subhrajit <subhrajittalukdar411@gmail.com>
2023-12-29 00:15:05 +0530

    Added a carousel section for what's latest feature

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 var navLinks = document.getElementById("navLinks");
-    function showMenu(){
-        navLinks.style.right = "0";
-    }
-    function hideMenu(){
-        navLinks.style.right = "-400px";
-    }
-    function toggleNav() {
-        var navLinks = document.querySelector('.nav-links');
-        navLinks.classList.toggle('open');
-      }
-      
-      // Attach the toggleNav function to the click event of the hamburger and close icons
-      document.querySelector('.bi.bi-list').addEventListener('click', toggleNav);
-      document.querySelector('.bi.bi-x-lg').addEventListener('click', toggleNav);
-      
-    //   scroll down animation
-    document.addEventListener('DOMContentLoaded', function () {
-        let welcomeContainer = document.querySelector('.welcome');
+function showMenu() {
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    navLinks.style.right = "-400px";
+}
+function toggleNav() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('open');
+}
 
-        function handleScroll() {
-            let scrollPosition = window.scrollY;
-            let elementPosition = welcomeContainer.offsetTop;
+// Attach the toggleNav function to the click event of the hamburger and close icons
+document.querySelector('.bi.bi-list').addEventListener('click', toggleNav);
+document.querySelector('.bi.bi-x-lg').addEventListener('click', toggleNav);
 
-            // Adjust the threshold value based on when you want the animation to start
-            let threshold = window.innerHeight / 2;
+//   scroll down animation
+document.addEventListener('DOMContentLoaded', function () {
+    let welcomeContainer = document.querySelector('.welcome');
 
-            if (scrollPosition >= elementPosition - threshold) {
-                welcomeContainer.style.opacity = '1';
-                welcomeContainer.style.transform = 'translateY(0)';
-            }
+    function handleScroll() {
+        let scrollPosition = window.scrollY;
+        let elementPosition = welcomeContainer.offsetTop;
+
+        // Adjust the threshold value based on when you want the animation to start
+        let threshold = window.innerHeight / 2;
+
+        if (scrollPosition >= elementPosition - threshold) {
+            welcomeContainer.style.opacity = '1';
+            welcomeContainer.style.transform = 'translateY(0)';
         }
+    }
 
-        // Initial check on page load
-        handleScroll();
+    // Initial check on page load
+    handleScroll();
 
-        // Attach the handleScroll function to the scroll event
-        window.addEventListener('scroll', handleScroll);
+    // Attach the handleScroll function to the scroll event
+    window.addEventListener('scroll', handleScroll);
+});
+
+// carousel for whats latest
+document.addEventListener('DOMContentLoaded', (event) => {
+    var myCarousel = document.querySelector('#carouselExampleCaptions')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 1000,
+        // pause: "hover"
     });
+});
 
-    // carousel for whats latest
-    $(document).ready(function(){
-        $("#carouselExampleIndicators").carousel({
-          interval: 2000,
-          pause: "hover"
-        });
-      });
 
-    
-    
