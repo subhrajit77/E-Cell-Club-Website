056afdd36563f0d0fce8ab71cab77d081ea2e95a
Subhrajit <subhrajittalukdar411@gmail.com>
2024-01-12 00:45:48 +0530

    adjusted some stylings

---
+++ js/team.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', function () {
+    let navbar = document.querySelector('.navbar');
+    if (window.scrollY > 100) { /* Change the value based on when you want the color to change */
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
+  let observer = new IntersectionObserver(function (entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, { threshold: 0.1 });
