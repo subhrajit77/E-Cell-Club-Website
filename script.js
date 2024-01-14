9e01466ef8cd687b413bb4e9e69512a66b4c90a3
Subhrajit <subhrajittalukdar411@gmail.com>
2024-01-14 19:45:50 +0530

    adjusted some minor scalings

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 window.addEventListener('scroll', function () {
-    let navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) { /* Change the value based on when you want the color to change */
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
-
+  let navbar = document.querySelector('.navbar');
+  if (window.scrollY > 100) { /* Change the value based on when you want the color to change */
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
 
 var navLinks = document.getElementById("navLinks");
 function showMenu() {
