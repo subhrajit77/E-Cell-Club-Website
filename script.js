c6590595adea570ad02f31991b829a86824c034e
Abhishek Kashyap <abhik5567@gmail.com>
2024-02-08 22:23:57 +0530

    remove unwanted js from main page

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   const navbar = document.getElementById('navbar');
   if (window.scrollY > 100) {
     navbar.classList.add('scrolled');
+    console.log('scrolled');
   } else {
     navbar.classList.remove('scrolled');
   }
 });
 
-var navLinks = document.getElementById("navLinks");
-function showMenu() {
-    navLinks.style.right = "0";
-}
-function hideMenu() {
-    navLinks.style.right = "-400px";
-}
-function toggleNav() {
-    var navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('open');
-}
+// var navLinks = document.getElementById("navLinks");
+// function showMenu() {
+//   navLinks.style.right = "0";
+// }
+// function hideMenu() {
 
-// Attach the toggleNav function to the click event of the hamburger and close icons
-document.querySelector('.bi.bi-list').addEventListener('click', toggleNav);
-document.querySelector('.bi.bi-x-lg').addEventListener('click', toggleNav);
+//   navLinks.style.right = "-400px";
+// }
+// function toggleNav() {
+//   var navLinks = document.querySelector('.nav-links');
+//   navLinks.classList.toggle('open');
+// }
+
+// document.querySelector('.bi.bi-list').addEventListener('click', toggleNav);
+// document.querySelector('.bi.bi-x-lg').addEventListener('click', toggleNav);
 
 //for rocket animation
 window.addEventListener('scroll', function () {
@@ -35,26 +36,26 @@ window.addEventListener('scroll', function () {
 
 //   scroll down animation
 document.addEventListener('DOMContentLoaded', function () {
-    let welcomeContainer = document.querySelector('.welcome');
+  let welcomeContainer = document.querySelector('.welcome');
 
-    function handleScroll() {
-        let scrollPosition = window.scrollY;
-        let elementPosition = welcomeContainer.offsetTop;
+  function handleScroll() {
+    let scrollPosition = window.scrollY;
+    let elementPosition = welcomeContainer.offsetTop;
 
-        // Adjust the threshold value based on when you want the animation to start
-        let threshold = window.innerHeight / 2;
+    // Adjust the threshold value based on when you want the animation to start
+    let threshold = window.innerHeight / 2;
 
-        if (scrollPosition >= elementPosition - threshold) {
-            welcomeContainer.style.opacity = '1';
-            welcomeContainer.style.transform = 'translateY(90px)';
-        }
+    if (scrollPosition >= elementPosition - threshold) {
+      welcomeContainer.style.opacity = '1';
+      welcomeContainer.style.transform = 'translateY(90px)';
     }
+  }
 
-    // Initial check on page load
-    handleScroll();
+  // Initial check on page load
+  handleScroll();
 
-    // Attach the handleScroll function to the scroll event
-    window.addEventListener('scroll', handleScroll);
+  // Attach the handleScroll function to the scroll event
+  window.addEventListener('scroll', handleScroll);
 });
 
 
@@ -63,11 +64,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // carousel for whats latest
 document.addEventListener('DOMContentLoaded', (event) => {
-    var myCarousel = document.querySelector('#carouselExampleCaptions')
-    var carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 100,
-        // pause: "hover"
-    });
+  var myCarousel = document.querySelector('#carouselExampleCaptions')
+  var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 100,
+    // pause: "hover"
+  });
 });
 
 //quote section
@@ -92,15 +93,15 @@ setInterval(updateQuote, 10000);
 
 
 // welcome container
-$(document).ready(function() {
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll > 200) {
-        $('.welcome-container').addClass('visible');
-      } else {
-        $('.welcome-container').removeClass('visible');
-      }
-    });
-  });
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     var scroll = $(window).scrollTop();
+//     if (scroll > 200) {
+//       $('.welcome-container').addClass('visible');
+//     } else {
+//       $('.welcome-container').removeClass('visible');
+//     }
+//   });
+// });
 
 
